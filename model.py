81ac4a90ede2857ca743986cfa073f94a4199fc2
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-04 16:04:15 +0500

    swiglu activation

--- model.py
+++ model.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 from torch.nn import functional as F
 import math
 
+class SwiGLU(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x) + x
+
 class RMSNorm(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -50,10 +54,11 @@ class FeedForward(nn.Module):
     def __init__(self, config) -> None:
         super().__init__()
         self.fc1 = nn.Linear(config['d_model'], 2048)
+        self.activation = SwiGLU()
         self.fc2 = nn.Linear(2048, config['d_model'])
     
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        x = self.activation(self.fc1(x))
         x = self.fc2(x)
         
         return x
