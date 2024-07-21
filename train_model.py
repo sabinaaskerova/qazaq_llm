bd649f486934ac93fdcf065844ab19e450b1f864
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-21 15:15:00 +0500

    vocab size, saving model

--- train_model.py
+++ train_model.py
@@ -16,7 +16,7 @@ class TextDataset(torch.utils.data.Dataset):
         self.tensor_text = tensor_text
 
     def __len__(self):
-        return self.tensor_text.shape[1]
+        return self.tensor_text.shape[1] - 1  # Last token has no next token
 
     def __getitem__(self, idx):
         return self.tensor_text[0][idx], self.tensor_text[0][idx + 1]
@@ -26,7 +26,7 @@ dataset = TextDataset(tensor_text)
 train_size = len(dataset)
 train_dataset = dataset
 
-batch_size = 256
+batch_size = 1024
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 print(f"Number of batches: {len(train_loader)}")
 print(f"Number of samples: {len(train_loader) * batch_size}")
@@ -42,7 +42,7 @@ config = {
     "n_heads": 8,
     "n_layers": 6,
     "n_positions": 1000,
-    "vocab_size": num_unique_tokens
+    "vocab_size": num_unique_tokens + 5
 }
 
 model = LanguageModel(config)
@@ -56,7 +56,7 @@ model.to(device)
 
 
 # Training loop
-num_epochs = 10
+num_epochs = 3
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
@@ -64,6 +64,7 @@ for epoch in range(num_epochs):
         inputs = inputs.unsqueeze(0)
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
+        
         outputs = model(inputs)
         outputs = outputs.squeeze(0)
         loss = criterion(outputs, targets)
@@ -79,8 +80,19 @@ for epoch in range(num_epochs):
     print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {average_loss}")
 
 
+
+
+model_save_path = "language_model.pth"
+torch.save(model, model_save_path)
+
+optimizer_save_path = "optimizer.pth"
+torch.save(optimizer.state_dict(), optimizer_save_path)
+
+print(f"Model and optimizer state saved to {model_save_path} and {optimizer_save_path}, respectively.")
+
 model.eval()
 
-start_tokens = torch.tensor([tensor_text[0][:20]], dtype=torch.long).to(device) # Starting with the first token
+start_tokens = tensor_text[0][:20].squeeze(0).unsqueeze(0).to(device)
 generated_text = model.generate(start_tokens, max_new_tokens=100, temperature=1.0)
-print(tokenizer.decode(generated_text.tolist()[0]))
+print(generated_text)
+# print(tokenizer.decode(generated_text.tolist()[0]))
