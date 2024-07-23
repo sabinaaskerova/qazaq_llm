0a59011ef6f7ae4c5ac9a471ba7996673e07d267
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-23 05:49:20 +0500

    checkpoints and early stopping

--- train_model.py
+++ train_model.py
@@ -54,6 +54,15 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
+checkpoint_interval = 1000
+
+# Early stopping parameters
+patience = 1
+best_loss = float('inf')
+epochs_no_improve = 0
+
+# Learning rate scheduler
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=patience)
 
 # Training loop
 num_epochs = 3
@@ -71,19 +80,34 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-        
-        # Print every 10 batches
+
+        if (batch_idx + 1) % checkpoint_interval == 0 or (batch_idx + 1) == len(train_loader):
+            torch.save(model.state_dict(), f'model_checkpoint_epoch{epoch+1}_batch{batch_idx+1}.pth')
+
         if (batch_idx + 1) % 10 == 0:
             print(f"Epoch {epoch+1}/{num_epochs}, Batch {batch_idx+1}/{len(train_loader)}, Batch Loss: {loss.item()}")
-    
+            
+
     average_loss = total_loss / len(train_loader)
     print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {average_loss}")
 
+    # Learning rate scheduler step
+    scheduler.step(average_loss)
+    print(f"Current learning rate: {optimizer.param_groups[0]['lr']}")
+
+    # Early stopping check
+    if average_loss < best_loss:
+        best_loss = average_loss
+        epochs_no_improve = 0
+    else:
+        epochs_no_improve += 1
+        if epochs_no_improve >= patience:
+            print(f"Early stopping triggered after {epoch+1} epochs.")
+            break
 
 
-
-model_save_path = "language_model.pth"
-torch.save(model, model_save_path)
+model_save_path = "language_model_state_dict.pth"
+torch.save(model.state_dict(), model_save_path)
 
 optimizer_save_path = "optimizer.pth"
 torch.save(optimizer.state_dict(), optimizer_save_path)
