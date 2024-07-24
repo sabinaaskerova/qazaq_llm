e03edf5b9786d4a2cdec4d773ff05d189ca3c77d
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-24 20:32:46 +0500

    directory for spm tokenizer

--- train_spm.py
+++ train_spm.py
@@ -1,4 +1,9 @@
 import sentencepiece as spm
+import os
 
 data_path = "data/"
+tokenizer_path = "tokenizer/"
+if not os.path.exists(tokenizer_path):
+    os.makedirs(tokenizer_path)
+    
 spm.SentencePieceTrainer.train(input=data_path+'kazakh_corpus.txt', model_prefix='tokenizer/m', vocab_size=1000)
