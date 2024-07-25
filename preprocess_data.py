6e20d0591276491e0c8528cb5b5873a17b92abc7
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-25 19:21:45 +0500

    streamed dataset loading

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,40 +1,39 @@
-
 from datasets import load_dataset
 import re
 import nltk
 nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 
-dataset = load_dataset('Nothingger/Kazakh-Literature-Collection')
+def clean_and_tokenize(texts, pattern):
+    cleaned_texts = []
+    for text in texts:
+        text = text.replace('\n', ' ')
+        text = text.replace('\xa0', ' ')
+        matches = pattern.findall(text)
+        cleaned_text = ''.join(matches)
+        sentences = sent_tokenize(cleaned_text)
+        cleaned_texts.extend(sentences)
+    return cleaned_texts
 
-wiki_dataset = load_dataset('amandyk/kazakh_wiki_articles')
-
-multidomain_dataset = load_dataset("kz-transformers/multidomain-kazakh-dataset")
-
-data = dataset['train']['text']
-data = data + wiki_dataset['train']['text']
-data = data + multidomain_dataset['train']['text']
 
 unique_chars = "!()*,-.012346789:;?«»ІАБВГДЕЖЗИКЛМНОПРСТУФХЧШЫЭЯабвгдежзийклмнопрстуфхцчшщъыьэюяіҒғҚқҢңҮүҰұһӘәӨө–—•−─"
-
 pattern = re.compile(f"[{re.escape(unique_chars)}\\s]+")
 
-cleaned_texts = []
 
-for text in data:
-    text = text.replace('\n', ' ')
-    text = text.replace('\xa0', ' ')
-    matches = pattern.findall(text)
-    cleaned_text = ''.join(matches)
-    sentences = sent_tokenize(cleaned_text)
-    cleaned_texts.extend(sentences)
+dataset = load_dataset('Nothingger/Kazakh-Literature-Collection', split='train', streaming=True)
+wiki_dataset = load_dataset('amandyk/kazakh_wiki_articles', split='train', streaming=True)
+multidomain_dataset = load_dataset("kz-transformers/multidomain-kazakh-dataset", split='train', streaming=True)
+
 
 data_path = "data/"
-output_file = data_path+'kazakh_corpus.txt'
+output_file = data_path + 'kazakh_corpus.txt'
 
+# Save cleaned data to file
 with open(output_file, 'w', encoding='utf-8') as f:
-    for sentence in cleaned_texts:
-        f.write(sentence.strip() + '\n')
+    for dataset_chunk in [dataset, wiki_dataset, multidomain_dataset]:
+        for example in dataset_chunk:
+            cleaned_texts = clean_and_tokenize([example['text']], pattern)
+            for sentence in cleaned_texts:
+                f.write(sentence.strip() + '\n')
 
 print(f"Cleaned data saved to {output_file}")
-
