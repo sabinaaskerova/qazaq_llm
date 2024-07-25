4b8e6d6fad7ea00cd35641046df1b16b250bd650
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-25 15:11:42 +0500

    tokenization using stream processing approach

--- save_tokens.py
+++ save_tokens.py
@@ -3,16 +3,26 @@ from tokenizer import Tokenizer
 
 data_path = "data/"
 
-with open(data_path+'kazakh_corpus.txt', 'r', encoding='utf-8') as f:
-        text = f.read()
+def data_generator(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            yield line.strip()
 
 tokenizer_path = "tokenizer/"
 tokenizer = Tokenizer(tokenizer_path+"m.model")
 
-text = tokenizer.encode(text)
-unique_tokens = set(text)
+tokenized_text = []
+
+for text in data_generator(data_path+'kazakh_corpus.txt'):
+     tokenized = tokenizer.encode(text)
+     tokenized_text.extend(tokenized)
+
+
+unique_tokens = set(tokenized_text)
 num_unique_tokens = len(unique_tokens)
-tensor_text = torch.tensor(text).unsqueeze(0)
+print(num_unique_tokens)
+
+tensor_text = torch.tensor(tokenized).unsqueeze(0)
 print("tensor_text.shape", tensor_text.shape)
 torch.save(tensor_text, tokenizer_path+"tensor_text.pt")
 torch.save(num_unique_tokens, tokenizer_path+"num_unique_tokens.pt")
