bbc4c0f6963dd877cc00015dc38a4f4d2a79f5f4
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-07-25 16:23:13 +0500

    multidomain kazakh dataset download

--- preprocess_data.py
+++ preprocess_data.py
@@ -9,8 +9,11 @@ dataset = load_dataset('Nothingger/Kazakh-Literature-Collection')
 
 wiki_dataset = load_dataset('amandyk/kazakh_wiki_articles')
 
+multidomain_dataset = load_dataset("kz-transformers/multidomain-kazakh-dataset")
+
 data = dataset['train']['text']
 data = data + wiki_dataset['train']['text']
+data = data + multidomain_dataset['train']['text']
 
 unique_chars = "!()*,-.012346789:;?«»ІАБВГДЕЖЗИКЛМНОПРСТУФХЧШЫЭЯабвгдежзийклмнопрстуфхцчшщъыьэюяіҒғҚқҢңҮүҰұһӘәӨө–—•−─"
 
